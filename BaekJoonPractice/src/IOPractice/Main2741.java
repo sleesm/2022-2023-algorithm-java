4b58981070aa255dcd11cf3b6a17039e369675af
SeongMinKwon <sleepingsm813@gmail.com>
2022-11-14 23:42:39 +0900

    [입출력] 백준 2741번 - N 찍기

---
+++ BaekJoonPractice/src/IOPractice/Main2741.java
@@ -0,0 +1,13 @@
+package IOPractice;
+import java.util.Scanner;
+
+class Main2741 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 1; i<= n; i++)
+			System.out.println(i);
+		sc.close();
+	}
+}
