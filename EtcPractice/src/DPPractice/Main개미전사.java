62635f64e495c692c1c6f5145208caffd3d0289f
SeongMinKwon <sleepingsm813@gmail.com>
2023-01-03 21:16:53 +0900

    [다이나믹 프로그래밍] 이것이 코딩 테스트다 - 개미전사
    d[i-2] 를 d[i-1]로 수정

--- EtcPractice/src/DPPractice/Main개미전사.java
+++ EtcPractice/src/DPPractice/Main개미전사.java
@@ -22,7 +22,7 @@ public class Main개미전사 {
 		dp[0] = arr[0];
 		dp[1] = Math.max(dp[0], arr[1]);
 		for(int i = 2; i < n; i++) {
-			dp[i]  = Math.max(dp[i-2] + arr[i], dp[i-2]);
+			dp[i]  = Math.max(dp[i-2] + arr[i], dp[i-1]);
 		}
 		System.out.println(dp[n-1]);
 	}
