70de4d6119b2ecf6fa4dbf5d13af933722928ac3
SeongMinKwon <sleepingsm813@gmail.com>
2023-01-03 21:13:33 +0900

    [다이나믹 프로그래밍] 백준 11727번 - 2×N 타일링 2

---
+++ BaekJoonPractice/src/DPPractice/Main11727.java
@@ -0,0 +1,20 @@
+package DPPractice;
+
+import java.util.Scanner;
+
+class Main11727 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		int[] dp = new int[1001];
+		dp[1] = 1;
+		dp[2] = 3;
+		for(int i = 3; i<= n; i++) {
+			dp[i] = (dp[i-2] * 2 + dp[i-1]) % 10007;
+		}
+		System.out.print(dp[n]);
+	}
+}
