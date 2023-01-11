3cecb9cadcfe38d3cc280823aa914a06a6ae388b
SeongMinKwon <sleepingsm813@gmail.com>
2023-01-11 20:41:01 +0900

    [탐색] 백준 9095번 - 1, 2, 3 더하기

--- BaekJoonPractice/src/DPPractice/Main9095.java
+++ BaekJoonPractice/src/DPPractice/Main9095.java
@@ -7,11 +7,6 @@ class Main9095 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		int[] nums = new int[t];
-		for(int i = 0; i<t; i++) {
-			nums[i] = sc.nextInt();
-		}
-		sc.close();
 		
 		int[] dp = new int[11];
 		dp[1] = 1;
@@ -20,8 +15,10 @@ class Main9095 {
 		for(int i = 4; i < 11; i++) {
 			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
 		}
+		
 		for(int i = 0; i< t; i++) {
-			System.out.println(dp[nums[i]]);
+			System.out.println(dp[sc.nextInt()]);
 		}
+		sc.close();
 	}
 }
