d1cabbd9d7825ef8b64a58591c59b5ffdb4fc01e
SeongMinKwon <sleepingsm813@gmail.com>
2023-03-28 21:29:41 +0900

    [다이나믹  프로그래밍] 이것이 코딩 테스트다 - 개미전사, 바닥공사 - 복습 겸 리팩토링

--- EtcPractice/src/DPPractice/Main개미전사.java
+++ EtcPractice/src/DPPractice/Main개미전사.java
@@ -11,19 +11,18 @@ public class Main개미전사 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] arr = new int[n];
+		int[] arr = new int[n + 1];
 		int[] dp = new int[101];
 		
-		for(int i = 0; i< n; i++) {
+		for(int i = 1; i<= n; i++) {
 			arr[i] = sc.nextInt();
 		}
 		sc.close();
 		
-		dp[0] = arr[0];
-		dp[1] = Math.max(dp[0], arr[1]);
-		for(int i = 2; i < n; i++) {
+		dp[1] = arr[1];
+		for(int i = 2; i <= n; i++) {
 			dp[i]  = Math.max(dp[i-2] + arr[i], dp[i-1]);
 		}
-		System.out.println(dp[n-1]);
+		System.out.println(dp[n]);
 	}
 }
